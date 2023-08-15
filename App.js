aa50dbdfc4e0e1ab3c05bf7857980bca7b741a04
Charles <babouwitonze921@gmail.com>
2023-08-15 20:25:36 +0200

    fixing scroll view

--- App.js
+++ App.js
@@ -1,80 +1,54 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,Button ,TextInput,FlatList,ScrollView} from 'react-native';
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
-  const [isOpen, setIsOpen]= useState(false)
-  const [text, setText]= useState("Live text")
+  const [isOpen, setIsOpen] = useState(false);
+  const [text, setText] = useState("Live text");
   const [people, setPeople] = useState([
-    { name: 'shaun', id: '1' },
-    { name: 'yoshi', id: '2' },
-    { name: 'mario', id: '3' },
-    { name: 'luigi', id: '4' },
-    { name: 'peach', id: '5' },
-    { name: 'toad', id: '6' },
-    { name: 'bowser', id: '7' },
+    { name: "shaun", id: "1" },
+    { name: "yoshi", id: "2" },
+    { name: "mario", id: "3" },
+    { name: "luigi", id: "4" },
+    { name: "peach", id: "5" },
+    { name: "toad", id: "6" },
+    { name: "bowser", id: "7" },
+    { name: "bowser", id: "8" },
+    { name: "bowser", id: "9" },
+    { name: "bowser", id: "10" },
+    { name: "bowser", id: "11" },
+    { name: "Gumpa", id: "12" },
   ]);
-  const enventHandler =()=>{
-    if (!isOpen){
-      setIsOpen(true)
-    }
-    else{
-      setIsOpen(false)
-      setText("Live text")
+  const enventHandler = () => {
+    if (!isOpen) {
+      setIsOpen(true);
+    } else {
+      setIsOpen(false);
+      setText("Live text");
     }
   }
   return (
     <View style={styles.container}>
-      
-<View style={styles.header}>
-  <Text style={styles.button}> Hello</Text>
-  
-</View>
-<View style={styles.header}>
-  <Text style={styles.button}> Hello 2</Text>
-  
-   <Button title='This is a button' onPress={enventHandler}/>
-  {isOpen && 
-       <View style={styles.button}>
-       <Text style={styles.button} > Hello 3</Text>
-       <TextInput 
-       multiline
-       inlineImageLeft='search_icon'
-       style={styles.input}
-       placeholder='Input holder'
-       onChangeText={(value)=>{
-        setText(value)
+      <Text>{text}</Text>
+        <View style={styles.list1}>
+        <ScrollView>
+          {people.map(user =>(
+            <View key={user.key}>
+              <Text style={styles.list2}>{user.name}</Text>
+            </View>
+          ))}
+          </ScrollView>
 
-       }} />
-          </View>
-  }
-</View>
-<Text>{text}</Text>
-<ScrollView>
-<View style={styles.list1} >
-  {people.map((user)=>{
-    return(
-    
-        <>
-        <Text style={styles.list} key={user.key}> {user.name}</Text>
-        </>
-        
-    )
+        </View>
 
-  })}
-</View>
-</ScrollView>
-{/* <View style={styles.list2}>
-<FlatList 
-        numColumns={2}
-        keyExtractor={(item) => item.id} 
-        data={people} 
-        renderItem={({ item }) => ( 
-          <Text style={styles.list}>{item.name}</Text>
-        )}
-      />
-</View> */}
     </View>
   );
 }
@@ -82,43 +56,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color:"#fff"
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#fff",
   },
-  header:{
+  header: {
     width: "80%",
-    backgroundColor:"blue",
-    color:"#fff"
+    backgroundColor: "blue",
+    color: "#fff",
   },
-  button:{
-    display:"flex",
-    backgroundColor:"pink",
-    color:"#fff",
-    borderColor:"red",
-    width:"77%"
+  button: {
+    display: "flex",
+    backgroundColor: "pink",
+    color: "#fff",
+    borderColor: "red",
+    width: "77%",
   },
-  input:{
-    borderWidth:1,
-    borderColor:"black",
-    width:"80%",
-    backgroundColor:"white"
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    width: "80%",
+    backgroundColor: "white",
   },
-  list1:{
-    borderWidth:2,
-    borderColor:'green'
+  list1: {
+    borderWidth: 2,
+    borderColor: "green",
   },
-  list2:{
-    borderWidth:3,
-    borderColor:'pink'
-  }
-  ,
-  list:{
-    paddingTop:5,
-    backgroundColor:"yellow",
-    padding:10,
-    fontSize:17
-  }
+  list2: {
+    borderWidth: 3,
+    borderColor: "pink",
+    fontSize: 25,
+ padding:30
+  },
+  list: {
   
+    backgroundColor: "yellow",
+    padding: 10,
+    
+  },
 });
